4654978de2bc53d3bdb268f7cd963e0cac8dce4e
jilani <jilani.oracledba@gmail.com>
2025-03-18 00:57:54 +0530

    Create deployApp.groovy

---
+++ vars/deployApp.groovy
@@ -0,0 +1,5 @@
+def call(String env) {
+    echo "Deploying to ${env} environment..."
+    #sh "deploy-script.sh ${env}"  // Replace with actual deployment command
+     sh "echo deploy inoto: ${env}"
+}
